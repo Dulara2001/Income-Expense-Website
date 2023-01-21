4b9d88968cd22391a7e1720413954d2876edff8f
dulara <it21014840@my.sliit.lk>
2023-01-21 09:43:39 +0530

    7th commit

--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -7,6 +7,7 @@ import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from validate_email import validate_email
+from django.contrib import messages
 
 # Create your views here.
 
@@ -37,4 +38,13 @@ class UsernameValidationView(View):
 
 class RegistrationView(View):
     def get(self, request):
+        return render(request, 'authentication/register.html')
+
+    def post(self, request):
+
+        messages.success(request, 'Success whatsapp success')
+        messages.warning(request, 'Success whatsapp warning')
+        messages.info(request, 'Success whatsapp request')
+        messages.error(request, 'Success whatsapp error')
+
         return render(request, 'authentication/register.html')
--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+from django.contrib import messages
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -125,6 +125,19 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'expenseswebsite/static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+
+MESSAGE_TAGS={
+    messages.ERROR: 'danger'
+}
+
+
+
+
+
+
+
+
 
 
 # Default primary key field type
