85bc5707f3d939455b6bfa225b59a4254235ee94
dulara <it21014840@my.sliit.lk>
2023-01-21 09:00:30 +0530

    6th commit

--- expenseswebsite/expenseswebsite/static/js/register.js
+++ expenseswebsite/expenseswebsite/static/js/register.js
@@ -2,6 +2,22 @@ const usernameField = document.querySelector("#usernameField");
 const feedBackArea = document.querySelector(".invalid_feedback");
 const emailField = document.querySelector("#emailField");
 const emailFeedBackArea = document.querySelector(".emailFeedBackArea");
+const passwordField = document.querySelector("#passwordField");
+const usernameSuccessOutput = document.querySelector(".usernameSuccessOutput");
+const showPasswordToggle = document.querySelector(".showPasswordToggle");
+const submitBtn = document.querySelector(".submit-btn");
+
+const handleToggleInput = (e) => {
+  if (showPasswordToggle.textContent === "SHOW") {
+    showPasswordToggle.textContent = "HIDE";
+    passwordField.setAttribute("type", "text");
+  } else {
+    showPasswordToggle.textContent = "SHOW";
+    passwordField.setAttribute("type", "password");
+  }
+};
+
+showPasswordToggle.addEventListener("click", handleToggleInput);
 
 
 emailField.addEventListener("keyup", (e) => {
@@ -19,17 +35,23 @@ emailField.addEventListener("keyup", (e) => {
       .then((data) => {
         console.log("data", data);
         if (data.email_error) {
-          
+          submitBtn.disabled = true;
           emailField.classList.add("is-invalid");
           emailFeedBackArea.style.display = "block";
           emailFeedBackArea.innerHTML = `<p>${data.email_error}</p>`;
-        } 
+        } else {
+          submitBtn.removeAttribute("disabled");
+        }
       });
   }
 });
 
 usernameField.addEventListener("keyup", (e) => {
   const usernameVal = e.target.value;
+
+  usernameSuccessOutput.style.display = "block";
+
+  usernameSuccessOutput.textContent = `Checking  ${usernameVal}`;
 
   usernameField.classList.remove("is-invalid");
   feedBackArea.style.display = "none";
@@ -46,8 +68,10 @@ usernameField.addEventListener("keyup", (e) => {
           usernameField.classList.add("is-invalid");
           feedBackArea.style.display = "block";
           feedBackArea.innerHTML = `<p>${data.username_error}</p>`;
-          
-        } 
+          submitBtn.disabled = true;
+        } else {
+          submitBtn.removeAttribute("disabled");
+        }
       });
   }
 });
